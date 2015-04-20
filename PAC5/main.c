ef7c95325ca5289a858c2d8c11fe41b35a4b2f10
Carles Saura <carles.saura@iizzii.com>
2015-04-20 18:00:30 +0200

    done

--- PAC5/main.c
+++ PAC5/main.c
@@ -30,6 +30,10 @@
     licensing and training services.
 */
 
+/* Library includes. */
+#include <stdio.h>
+#include <stdlib.h>
+
 /* FreeRTOS.org includes. */
 #include "FreeRTOS.h"
 #include "task.h"
@@ -45,7 +49,7 @@ only a single instance is created of the receiver task. */
 static void vSenderTask( void *pvParameters );
 static void vReceiverTask( void *pvParameters );
 
-#define _MAX_NUMBER_STACKS_IN_QUEUE 5
+#define _MAX_NUMBER_STACKS_IN_QUEUE 20
 
 /*-----------------------------------------------------------*/
 
@@ -65,28 +69,30 @@ int main( void )
 	/* Initiate the UART port to print */
 	Log_start(3, 115200);
     /* The queue is created to hold a maximum of 5 long values. */
-    xQueue = xQueueCreate( _MAX_NUMBER_STACKS_IN_QUEUE, sizeof( long ) );
+    xQueue = xQueueCreate( _MAX_NUMBER_STACKS_IN_QUEUE, sizeof( int ) );
     /* Before a semaphore is used it must be explicitly created. */
 	xMutex = xSemaphoreCreateMutex();
+	//vSemaphoreCreateBinary( xMutex );
 	/* Take the semaphore to prevent acces to the Queue when is full */
-	xSemaphoreTake( xMutex, portMAX_DELAY );
+	//xSemaphoreTake( xMutex, portMAX_DELAY );
 	/* The tasks are going to use a pseudo random delay, seed the random number
 	generator. */
 	srand( 435 );
 
-	if( xQueue != NULL )
+	if( xQueue != NULL && xMutex != NULL )
 	{
+		//xSemaphoreTake( xMutex, 10 );
 		/* Create two instances of the task that will write to the queue.  The
 		parameter is used to pass the value that the task should write to the queue,
 		so one task will continuously write 100 to the queue while the other task
 		will continuously write 200 to the queue.  Both tasks are created at
 		priority 1. */
-		xTaskCreate( vSenderTask, "Sender1", 240, ( void * ) 150, 2, NULL );
-		xTaskCreate( vSenderTask, "Sender2", 240, ( void * ) 250, 2, NULL );
+		xTaskCreate( vSenderTask, "Sender1", 240, ( void * ) 1, 2, NULL );
+		xTaskCreate( vSenderTask, "Sender2", 240, ( void * ) 2, 2, NULL );
 
 		/* Create the task that will read from the queue.  The task is created with
 		priority 2, so above the priority of the sender tasks. */
-		xTaskCreate( vReceiverTask, "Receiver", 240, NULL, 1, NULL );
+		xTaskCreate( vReceiverTask, "Receiver", 240, NULL, 3, NULL );
 
 		/* Start the scheduler so the created tasks start executing. */
 		vTaskStartScheduler();
@@ -109,20 +115,22 @@ int main( void )
 
 static void vSenderTask( void *pvParameters )
 {
-long lValueToSend;
+long instance_id;
 portBASE_TYPE xStatus;
 char str[30];
-int toSend = rand();
+int toSend;
 
 	/* Two instances are created of this task so the value that is sent to the
 	queue is passed in via the task parameter rather than be hard coded.  This way
 	each instance can use a different value.  Cast the parameter to the required
 	type. */
-	lValueToSend = ( long ) pvParameters;
+	instance_id = ( long ) pvParameters;
 
 	/* As per most tasks, this task is implemented within an infinite loop. */
 	for( ;; )
 	{
+		/* The random generated number to enqueue */
+		toSend = rand();
 		/* The first parameter is the queue to which data is being sent.  The
 		queue was created before the scheduler was started, so before this task
 		started to execute.
@@ -138,15 +146,31 @@ int toSend = rand();
 		if( xStatus != pdPASS )
 		{
 			/* We could not write to the queue because it was full. Send a message to inform about it */
-			vPrintString( "Could not send to the queue.\r\n" );
+			vPrintStringAndNumber( "Could not send to the queue.\r\n", instance_id );
 			Log_start(3, 115200);
-			Log_log( str, "Could not send to the queue. Sender %d\n. Value: %d\n", lValueToSend, toSend );
+			Log_log( str, "Could not send to the queue. Sender %d\n. Value: %d\n", instance_id, toSend );
+			/* Give the semaphore to mark as resource busy */
+			xSemaphoreTake( xMutex, 0 );
 			/* Wait until Queue will be empty */
-			while ( xSemaphoreTake( xMutex, portMAX_DELAY ) != pdTRUE){}
-			xStatus = xQueueSendToBack( xQueue, &toSend, portMAX_DELAY );
+			while ( xSemaphoreTake( xMutex, 100 ) != pdTRUE){
+				//vPrintStringAndNumber( "Sleeping! Sender: ", instance_id );
+				//taskYIELD();
+			}
+			vPrintStringAndNumber( "Awake! Sender: ", instance_id );
+			xStatus = xQueueSendToBack( xQueue, &toSend, 0 );
 			xSemaphoreGive( xMutex );
+
+			/* Allow the other sender task to execute. */
+			//taskYIELD();
+			//vTaskDelay( 1000 );
 		}
-		toSend = rand();
+		else{
+			//xSemaphoreGive( xMutex );
+			vPrintStringAndNumber( "Queued! Sender: ", instance_id );
+			/* Allow the other sender task to execute. */
+			//taskYIELD();
+			//vTaskDelay( 100/portTICK_RATE_MS );
+		}
 		/* Allow the other sender task to execute. */
 		taskYIELD();
 	}
@@ -171,6 +195,7 @@ char str[30];
 				case 0:
 					vPrintString( "Queue is empty\r\n" );
 					Log_log( str, "Queue is empty\r\n" );
+					vTaskDelay( xTicksToWait );
 					break;
 				case _MAX_NUMBER_STACKS_IN_QUEUE:
 					while ( uxQueueMessagesWaiting( xQueue ) != 0 ){
@@ -181,49 +206,53 @@ char str[30];
 							Log_log( str, "The Queue was full. Value = %d\n", lReceivedValue );
 					}
 					xSemaphoreGive( xMutex );
+
 					/* Allow the sender task to execute. */
 					//taskYIELD();
-					xSemaphoreTake( xMutex, portMAX_DELAY );
+					//xSemaphoreTake( xMutex, portMAX_DELAY );
 					break;
-				default: break;
+				default:
+					/* The first parameter is the queue from which data is to be received.  The
+					queue is created before the scheduler is started, and therefore before this
+					task runs for the first time.
+
+					The second parameter is the buffer into which the received data will be
+					placed.  In this case the buffer is simply the address of a variable that
+					has the required size to hold the received data.
+
+					the last parameter is the block time � the maximum amount of time that the
+					task should remain in the Blocked state to wait for data to be available should
+					the queue already be empty. */
+					//xSemaphoreGive( xMutex );
+					xStatus = xQueueReceive( xQueue, &lReceivedValue, xTicksToWait );
+
+					if( xStatus == pdPASS )
+					{
+						/* Data was successfully received from the queue, print out the received
+						value. */
+						vPrintStringAndNumber( "Received = ", lReceivedValue );
+						Log_start(3, 115200);
+						Log_log( str, "Received = %d", lReceivedValue );
+					}
+					break;
 				}
-		if( uxQueueMessagesWaiting( xQueue ) != 0 )
+/*		if( uxQueueMessagesWaiting( xQueue ) != 0 )
 		{
 			vPrintString( "Queue should have been empty!\r\n" );
 			Log_start(3, 115200);
 			Log_log( str, "Queue should have been empty!\r\n" );
-		}
+		}*/
 
-		/* The first parameter is the queue from which data is to be received.  The
-		queue is created before the scheduler is started, and therefore before this
-		task runs for the first time.
 
-		The second parameter is the buffer into which the received data will be
-		placed.  In this case the buffer is simply the address of a variable that
-		has the required size to hold the received data.
-
-		the last parameter is the block time � the maximum amount of time that the
-		task should remain in the Blocked state to wait for data to be available should
-		the queue already be empty. */
-		xStatus = xQueueReceive( xQueue, &lReceivedValue, xTicksToWait );
-
-		if( xStatus == pdPASS )
-		{
-			/* Data was successfully received from the queue, print out the received
-			value. */
-			vPrintStringAndNumber( "Received = ", lReceivedValue );
-			Log_start(3, 115200);
-			Log_log( str, "Received = %d", lReceivedValue );
-		}
-		else
-		{
-			/* We did not receive anything from the queue even after waiting for 100ms.
-			This must be an error as the sending tasks are free running and will be
-			continuously writing to the queue. */
-			vPrintString( "Could not receive from the queue.\r\n" );
-			Log_start(3, 115200);
-			Log_log( str, "Could not receive from the queue.\r\n" );
-		}
+//		else
+//		{
+//			/* We did not receive anything from the queue even after waiting for 100ms.
+//			This must be an error as the sending tasks are free running and will be
+//			continuously writing to the queue. */
+//			vPrintString( "Could not receive from the queue.\r\n" );
+//			Log_start(3, 115200);
+//			Log_log( str, "Could not receive from the queue.\r\n" );
+//		}
 	}
 }
 /*-----------------------------------------------------------*/
